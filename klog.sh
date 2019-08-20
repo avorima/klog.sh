6c09ef16fbe43d982381dd7b3710e7a5af2773f1
Mario Valderrama <woldy401@gmail.com>
2019-08-20 22:09:16 +0200

    Implement klog.Warning*

--- klog.sh
+++ klog.sh
@@ -22,6 +22,21 @@ klog::Infof() {
     echo "I$(date +"%m%d") $(date +$DATESTR) $$ $(basename "${c[1]}"):${c[0]}] $msg"
 }
 
+klog::Warning() {
+    local c=($(caller))
+    local a=("$@")
+    local msg=$(IFS=; echo "${a[*]}")
+    echo "W$(date +"%m%d") $(date +$DATESTR) $$ $(basename "${c[1]}"):${c[0]}] $msg" 1>&2
+}
+
+klog::Warningf() {
+    local c=($(caller))
+    local msg
+    local a=("$@")
+    msg=$(printf "$1" ${a[@]:1})
+    echo "W$(date +"%m%d") $(date +$DATESTR) $$ $(basename "${c[1]}"):${c[0]}] $msg" 1>&2
+}
+
 klog::Error() {
     local c=($(caller))
     local a=("$@")
