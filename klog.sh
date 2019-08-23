1caf50a8e024a85e2375da360b15968df57404f0
Mario Valderrama <woldy401@gmail.com>
2019-08-23 13:17:07 +0200

    Implement Fatal/f/ln

--- klog.sh
+++ klog.sh
@@ -16,6 +16,19 @@ __writeHeader() {
         "$(basename "${c[2]}")" "${c[0]}" 1>&2
 }
 
+__stacktrace() {
+    if (( ${#FUNCNAME[@]} > 2 )); then
+        echo "Stacktrace:"
+        # Start at 2 because we're calling this from klog::Fatal*
+        local stack_size=${#FUNCNAME[@]}
+        for (( i=2; i<stack_size; i++ )); do
+            local func="${FUNCNAME[$i]}"
+            [[ x$func == x ]] && func=MAIN
+            echo "  $(realpath "${BASH_SOURCE[$i]}") $func:${BASH_LINENO[$((i - 1))]}"
+        done
+    fi
+}
+
 klog::Info() {
     __writeHeader "I"
     echo -e "$(__join "" "$@")"
@@ -62,4 +75,26 @@ klog::Errorf() {
 klog::Errorln() {
     __writeHeader "E"
     echo -e "$@" 1>&2
+}
+
+klog::Fatal() {
+    __writeHeader "F"
+    echo -e "$(__join "" "$@")" 1>&2
+    __stacktrace 1>&2
+    exit 255
+}
+
+klog::Fatalf() {
+    __writeHeader "F"
+    local a=("$@")
+    echo -e "$(printf "$1" ${a[@]:1})" 1>&2
+    __stacktrace 1>&2
+    exit 255
+}
+
+klog::Fatalln() {
+    __writeHeader "F"
+    echo -e "$@" 1>&2
+    __stacktrace 1>&2
+    exit 255
 }
