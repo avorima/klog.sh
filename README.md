bcbbc7ff4862c7cc20c327561cc02704589f659b
Mario Valderrama <woldy401@gmail.com>
2019-08-23 13:17:27 +0200

    Update tests and docs

--- README.md
+++ README.md
@@ -2,14 +2,16 @@
 
 [klog](https://github.com/kubernetes/klog) for bash (only bash for now).
 
-Sourcing `klog.sh` will expose the following functions:
+Sourcing `klog.sh` will expose functions for INFO, WARNING, ERROR and FATAL.
+Each level will have the following functions (where `X` is the capitalized level):
 
-- `klog::Info ...`
-- `klog::Infof format ...`
-- `klog::Infoln ...`
-- `klog::Warning ...`
-- `klog::Warningf format ...`
-- `klog::Warningln ...`
-- `klog::Error ...`
-- `klog::Errorf format ...`
-- `klog::Errorln ...`
+- `klog::X`: Simply print the arguments. The arguments will be joined together without spaces
+- `klog::Xf`: Used in the same way as `printf`
+- `klog::Xln`: Same as `klog::X`, but arguments will be separated by spaces.
+
+The FATAL logger will print a stack trace and exit with 255.
+
+All functions will log to `stderr` and treat newlines in their arguments correctly.
+However they won't remove trailing newlines, like the actual klog functions would do.
+
+Check out the `test.sh` file for some examples.
--- test.sh
+++ test.sh
@@ -10,8 +10,50 @@ assert_that_has_message() {
     fi
 }
 
+assert_that_failed_ok() {
+    local exit_code=$?
+    if (( exit_code != 255 )); then
+        caller
+        echo "Wrong exit code"
+        exit 1
+    fi
+}
+
 assert_that_has_message "$(klog::Info "foo" "bar" 2>&1)" "foobar"
 assert_that_has_message "$(klog::Info "foo " "bar" 2>&1)" "foo bar"
+assert_that_has_message "$(klog::Info "foo\n" "bar" 2>&1)" "foo"
 assert_that_has_message "$(klog::Infof "foobar" 2>&1)" "foobar"
 assert_that_has_message "$(klog::Infof "foo%s" "bar" 2>&1)" "foobar"
 assert_that_has_message "$(klog::Infoln "foo" "bar" 2>&1)" "foo bar"
+
+(
+    klog::Fatal "panic"
+)
+assert_that_failed_ok
+
+func1() {
+    func2
+}
+
+func2() {
+    klog::Fatal "panic"
+}
+
+# Verify that stacktrace contains the correct elements
+stacktrace=()
+while read -r line; do
+    [[ $line == "Stacktrace:" ]] && continue
+    stacktrace+=("$line")
+done < <(
+    func1 2>&1
+)
+
+if [[ ${#stacktrace[@]} -eq 0 ]]; then
+    echo "Expected stack trace to be printed"
+    exit 1
+fi
+
+assert_that_has_message "${stacktrace[0]}" "panic"
+assert_that_has_message "${stacktrace[1]}" "func2"
+assert_that_has_message "${stacktrace[2]}" "func1"
+assert_that_has_message "${stacktrace[3]}" "main"
